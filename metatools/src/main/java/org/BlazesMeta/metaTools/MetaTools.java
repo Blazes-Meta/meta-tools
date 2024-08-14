5081a29e4ea2c84dcb6459db68932deb9ea70b6c
Annhilati <antbe.artis@web.de>
2024-08-14 14:30:42 +0200

    Sync

---
+++ metatools/src/main/java/org/BlazesMeta/metaTools/MetaTools.java
@@ -0,0 +1,17 @@
+package org.BlazesMeta.metaTools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MetaTools extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
