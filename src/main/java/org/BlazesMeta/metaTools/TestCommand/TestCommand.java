0a4bfd85503934d71f28a3e3e2ccafdb6f94c124
Annhilati <antbe.artis@web.de>
2024-08-14 18:55:00 +0200

    Update TestCommand.java

--- src/main/java/org/BlazesMeta/metaTools/TestCommand/TestCommand.java
+++ src/main/java/org/BlazesMeta/metaTools/TestCommand/TestCommand.java
@@ -1,5 +1,6 @@
 package org.BlazesMeta.metaTools.TestCommand;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,6 +9,7 @@ public class TestCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "tellraw @a [\"\",{\"text\":\"[\",\"color\":\"white\"},{\"text\":\"Broadcast\",\"color\":\"gold\"},{\"text\":\"] Alle Spieler in der Oberwelt werden gebeten, den Server zu verlassen oder zu schlafen, damit die Nacht übersprungen werden kann.\",\"color\":\"white\"}]");
         commandSender.sendMessage("Test");
         return false;
     }
