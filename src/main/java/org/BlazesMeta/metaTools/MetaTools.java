8a315a09cef3de2338f954e859ae34968723e7b0
Annhilati <antbe.artis@web.de>
2024-08-14 14:55:15 +0200

    Sync

--- src/main/java/org/BlazesMeta/metaTools/MetaTools.java
+++ src/main/java/org/BlazesMeta/metaTools/MetaTools.java
@@ -1,12 +1,13 @@
 package org.BlazesMeta.metaTools;
 
+import org.BlazesMeta.metaTools.commands.TestCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MetaTools extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getCommand("test").setExecutor(new TestCommand());
 
     }
 
